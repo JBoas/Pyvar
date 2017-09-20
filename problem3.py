1f9596a50981fcb9988c947aa544f7e852f4373c
JBoas <jboas@sjnma.org>
2017-09-19 21:01:23 -0500

    problem3 done

---
+++ problem3.py
@@ -0,0 +1,9 @@
+print("Please enter you full name")
+fullname = input()
+names = fullname.split()
+
+print(names)
+
+print("First Name:",(names[0]))
+print("Middle Name:",(names[1]))
+print("Last Name:",(names[2]))
