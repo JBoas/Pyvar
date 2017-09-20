cefe9fb11d84432d11b56f7af7d7509a9e92ab79
JBoas <jboas@sjnma.org>
2017-09-19 20:15:06 -0500

    problem1 done

---
+++ problem1.py
@@ -0,0 +1,10 @@
+number1 = input("Enter number:")
+number2 = input("Enter number:")
+number3 = input("Enter number:")
+number4 = input("Enter number:")
+number5 = input("Enter number:")
+
+sum = int(number1) +int(number2) +int(number3) +int(number4) + int(number5)
+print(sum)
+print("The average of those numbers is")
+print(sum/5)
